928471251efbe9cb7bf1d7ca05d7f4b67e28ffba
Dawid Grabowski <kontakt@grabowskidawid.com>
2023-08-20 22:29:29 +0200

    fix: redirect

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,7 +4,7 @@ import { type ReactElement } from "react";
 export default function Register(): ReactElement {
   return (
     <section className='w-screen min-h-screen flex items-center justify-center pb-16'>
-      <SignUp path='/register' routing='path' />
+      <SignUp path='/register' />
     </section>
   );
 }
