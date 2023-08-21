ba2169cb8ac82538ddf002df86f0826a66b4a937
Dawid Grabowski <kontakt@grabowskidawid.com>
2023-08-21 19:27:29 +0200

    add: debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/privacy-policy", "/login", "/register"],
+  debug: true,
 });
 
 export const config = {
