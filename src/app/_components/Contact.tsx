66359a5bda12300c963b811a659e05ee15295de3
Dawid Grabowski <kontakt@grabowskidawid.com>
2023-08-23 18:16:11 +0200

    fix: env variables

--- src/app/_components/Contact.tsx
+++ src/app/_components/Contact.tsx
@@ -34,10 +34,10 @@ export function Contact(): ReactElement {
 
     await emailjs
       .sendForm(
-        process.env.EMAILJS_SERVICE_ID ?? "",
-        process.env.EMAILJS_TEMPLATE_ID ?? "",
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID ?? "",
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID ?? "",
         form.current,
-        process.env.EMAILJS_PUBLIC_KEY ?? ""
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY ?? ""
       )
       .then(
         (result) => {
