edd2866cf73f086abfabcec7631f3031e6d4ea69
Dawid Grabowski <kontakt@grabowskidawid.com>
2023-08-23 17:40:21 +0200

    fix: urls

--- src/app/_data/url.ts
+++ src/app/_data/url.ts
@@ -1,2 +1,8 @@
-export const APP_URL =
-  process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
+let URL = "http://localhost:3000";
+
+if (typeof window !== "undefined") {
+  const currentUrl = window.location.href;
+  URL = currentUrl;
+}
+
+export const APP_URL = process.env.NEXT_PUBLIC_VERCEL_URL || URL;
