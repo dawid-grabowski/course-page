5fa2ccbdbdac4a1386645c53895a9b0216f3f9cb
Dawid Grabowski <kontakt@grabowskidawid.com>
2023-08-28 22:00:02 +0200

    fix: navbar styling on register page

--- src/app/register/[[...register]]/page.tsx
+++ src/app/register/[[...register]]/page.tsx
@@ -4,7 +4,7 @@ import { type ReactElement } from "react";
 
 export default async function Register(): Promise<ReactElement> {
   return (
-    <section className='w-screen min-h-screen flex items-center justify-center flex-col top-full pb-16'>
+    <section className='w-screen min-h-screen flex items-center justify-center flex-col top-full pt-24 pb-16'>
       <SignUp />
       <Footer />
     </section>
