362d900a3369b7c83db72893ff20588f57524f8d
Dawid Grabowski <kontakt@grabowskidawid.com>
2023-08-29 23:52:54 +0200

    add: navbar

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { SVGProps, useState, type ReactElement } from "react";
 import { navigation, type Navigation } from "../_data/navigation";
 import { SVGLogo } from "./SVGLogo";
 
-export function Navbar(props: SVGProps<SVGSVGElement>): ReactElement {
+export function Navbar(): ReactElement {
   const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
   const { isLoaded, isSignedIn } = useUser();
 
@@ -33,13 +35,11 @@ export function Navbar(props: SVGProps<SVGSVGElement>): ReactElement {
         <div className='flex lg:hidden'>
           <button
             type='button'
-            className='inline-flex items-center justify-center rounded-md text-indigo-800'
+            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-zinc-200'
             onClick={() => setMobileMenuOpen(true)}
           >
-            <span className='sr-only'>Otwórz menu główne</span>
-            {/* <MenuButton
-              toggle={() => setMobileMenuOpen((prevState) => !prevState)}
-            /> */}
+            <span className='sr-only'>Open main menu</span>
+            <Bars3Icon className='h-6 w-6' aria-hidden='true' />
           </button>
         </div>
         <div className='hidden lg:flex lg:gap-x-12'>
@@ -79,6 +79,51 @@ export function Navbar(props: SVGProps<SVGSVGElement>): ReactElement {
           )}
         </div>
       </motion.nav>
+      <Dialog
+        as='div'
+        className='lg:hidden'
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <Dialog.Panel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-neutral-900 shadow-lg px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
+          <div className='flex items-center justify-between'>
+            <Link href='/' className='-m-1.5 p-1.5'>
+              <span className='sr-only'>Dawid Grabowski - GrabIT</span>
+              <SVGLogo className='h-6 w-auto' />
+            </Link>
+            <button
+              type='button'
+              className='-m-2.5 rounded-md p-2.5 text-zinc-200'
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className='sr-only'>Close menu</span>
+              <XMarkIcon className='h-6 w-6' aria-hidden='true' />
+            </button>
+          </div>
+          <div className='mt-6 flow-root'>
+            <div className='-my-6 divide-y divide-zinc-400/10'>
+              <div className='space-y-2 py-6'>
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-zinc-200 hover:bg-neutral-800 transition-colors'
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className='py-6'>
+                <SignInButton>
+                  <span className='-mx-3 cursor-pointer block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-zinc-200 hover:bg-neutral-800 transition-colors'>
+                    Zaloguj się
+                  </span>
+                </SignInButton>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </header>
   );
 }
